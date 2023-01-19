c5e75e29cd411234fe97af4f7a2ef3640f6d1a25
Navin Paudel <pnavin607@gmail.com>
2023-01-19 10:34:01 +0545

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css"
+import "./App.css"
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./components/Register";
